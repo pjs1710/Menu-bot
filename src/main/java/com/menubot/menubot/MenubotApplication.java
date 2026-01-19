b53eda799cf4b04a274ebff0806f882a85fb0064
pjs1710 <timer973@naver.com>
2026-01-19 13:46:12 +0900

    menu-bot/feat: first init

---
+++ src/main/java/com/menubot/menubot/MenubotApplication.java
@@ -0,0 +1,13 @@
+package com.menubot.menubot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenubotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenubotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menubot/menubot/MenubotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menubot.menubot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenubotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
